c19cf8e3634978905912af10d7c8c00c138863ab
thr0w <thr0w@users.noreply.github.com>
2015-11-14 08:49:05 -0200

    estatisticas

--- client/nao_logado.js
+++ client/nao_logado.js
@@ -1,6 +1,14 @@
 Template.nao_logado.rendered = function() {
   setTimeout(function()
   {
-    $('#login-buttons-facebook > span').text('Facebook');
+    $('#login-buttons-facebook > span').text('Acessar com o Facebook');
   },100)
+  setTimeout(function()
+  {
+    $('#login-buttons-facebook > span').text('Acessar com o Facebook');
+  },1500)
+  setTimeout(function()
+  {
+    $('#login-buttons-facebook > span').text('Acessar com o Facebook');
+  },3000)
 };
