32efb712e609824f5c6f2a0d5d36309c7cef2688
thr0w <thr0w@users.noreply.github.com>
2015-11-19 01:51:58 -0200

    sõ mapa

--- client/conteudo.js
+++
@@ -1,9 +0,0 @@
-Meteor.autorun(()=>Meteor.subscribe('meu_login', Meteor.userId()))
-Meteor.autorun(()=>Meteor.subscribe('minha_assinatura', Meteor.userId()))
-Meteor.subscribe('estatisticas')
-
-Template.conteudo.helpers({
-  logado: () => Meteor.user() != null,
-  jaAssinou: () => Assinaturas.find({userId: Meteor.userId()}).count() == 1
-});
-
--- client/estatisticas.js
+++
@@ -1,4 +0,0 @@
-Template.estatisticas.helpers({
-  qtde_assinaturas: () => Totais.findOne({name: 'qtde_assinaturas'}).value
-});
-
